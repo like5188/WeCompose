e11aac2e03e7456657a22834b14fb203d7479e17
like5188 <260864866@qq.com>
2021-12-21 14:53:13 +0800

    完善

--- app/src/main/java/com/like/wecompose/ui/ChatPage.kt
+++ app/src/main/java/com/like/wecompose/ui/ChatPage.kt
@@ -2,28 +2,35 @@ package com.like.wecompose.ui
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.like.wecompose.WeViewModel
 import kotlin.math.roundToInt
 
 @Composable
-fun ChatPage() {
+fun ChatPage(modifier: Modifier = Modifier) {
     val viewModel: WeViewModel = viewModel()
     val offsetPercentX by animateFloatAsState(targetValue = if (viewModel.chatting) 0f else 1f)
     val offsetPercentY by animateFloatAsState(targetValue = if (viewModel.chatting) 0f else 1f)
-    Box(
-        modifier = Modifier
-            .offsetPercent(offsetPercentX, offsetPercentY)
-            .background(Color.Red)
-            .fillMaxSize()
-    )
+    val chat = viewModel.currentChat
+    if (chat != null) {
+        Column(
+            modifier = modifier
+                .offsetPercent(offsetPercentX, offsetPercentY)
+                .background(MaterialTheme.colors.background)
+                .fillMaxSize()
+        ) {
+            WeTopBar(title = chat.friend.name) {
+                viewModel.endChat()
+            }
+        }
+    }
 }
 
 fun Modifier.offsetPercent(offsetPercentX: Float = 0f, offsetPercentY: Float = 0f): Modifier = this.layout { measurable, constraints ->
